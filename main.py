e2914d79a767f42b03293c9a70fb1f76248a7122
axl <dj.axl.borges@gmail.com>
2026-01-05 16:27:54 -0600

    fix: checks for translation keys before writing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # IMPORTANT
 # Must have structure: 
 #   - namespace: <studioName>_<projectName>
-#   - directory example: ...<projectNameCamel>/<projectNameCamel>-BP/items/<studioName>/<projectName>/...
+#   - directory example: ...<projectNameCamel>/<projectNameCamel>-BP/...
 #   - file name, identifier and actual name are similar: awesome_sword.item.json --> <namespace>:awesome_sword --> Awesome Sword
 #
 # Created by QuantumNek0
@@ -47,18 +47,19 @@ def writeKeys(lang_file: TextIO, files: list[str], namespace: str, mode: str):
         match mode:
 
             case "item":
-                key = f"item.{namespace}:{identifier}=" + natural_name + '\n'
+                key = f"item.{namespace}:{identifier}="
 
             case "block":
-                key = f"tile.{namespace}:{identifier}.name=" + natural_name + '\n'
+                key = f"tile.{namespace}:{identifier}.name="
 
             case "entity":
-                key = f"entity.{namespace}:{identifier}.name=" + natural_name + '\n'
+                key = f"entity.{namespace}:{identifier}.name="
 
             case _:
                 raise ValueError("Invalid mode.")
         
-        if (key not in contents): lang_file.write(key)
+        if (key not in contents): lang_file.write(key + natural_name + '\n')
+
 
 def getAllFiles(directory: str) -> tuple[list[str], list[str], list[str]]:
 
@@ -67,6 +68,7 @@ def getAllFiles(directory: str) -> tuple[list[str], list[str], list[str]]:
     block_files = getFiles(directory + "/blocks", ".json")
 
     return item_files, block_files, entity_files
+
 
 def getFiles(directory: str, extension: str) -> list[str]:
     files: list[str] = []
@@ -78,26 +80,19 @@ def getFiles(directory: str, extension: str) -> list[str]:
 
     return files
 
+
 def formatFileName(filename: str) -> tuple[str, str]:
-    # 1. manipulate the input argument in a variable named 'natural_name'
     natural_name = filename
     
-    # 2. remove the file extension (probably .json)
+    # remove the file extension (probably .json)
     natural_name = os.path.splitext(natural_name)[0]
-    
-    # 3. remove the second extension if any (either .item or .block)
+    # remove the second extension if any (either .item or .block)
     natural_name = os.path.splitext(natural_name)[0]
-    
-    # 3.1. save the string at its current state in a variable named 'identifier'
     identifier = natural_name
-    
-    # 4. replace underscores with spaces
-    natural_name = natural_name.replace('_', ' ')
-    
-    # 5. capitalize the start of every word
+
+    natural_name = natural_name.replace('_', ' ')    
     natural_name = natural_name.title()
     
-    # 6. return both 'natural_name' and 'identifier'
     return identifier, natural_name
 
 
